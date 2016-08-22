68064ecfc40051d6b2c0eda37a6f94e5ec11e1ae
turbio <masongclayton@gmail.com>
2016-08-21 20:18:16 -0700

    remove all archive disk storage

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -9,33 +9,8 @@ var client = redis.createClient();
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
-/*
- * You will need to reuse the same paths many times over in the course of this sprint.
- * Consider using the `paths` object below to store frequently used file paths. This way,
- * if you move any files, you'll only need to change your code in one place! Feel free to
- * customize it in any way you wish.
- */
 var indexIncrScript = 'return redis.call("zadd", "queue", redis.call("incr", "index"), KEYS[1])';
 var indexIncrHash = client.script('load', indexIncrScript);
-
-exports.paths = {
-  siteAssets: path.join(__dirname, '../web/public'),
-  archivedSites: path.join(__dirname, '../archives/sites'),
-
-  requested: path.join(__dirname, '../archives/sites.txt'),
-  pending: path.join(__dirname, '../archives/pending.txt'),  
-  done: path.join(__dirname, '../archives/done.txt')
-};
-
-// Used for stubbing paths for tests, do not modify
-exports.initialize = function(pathsObj) {
-  _.each(pathsObj, function(path, type) {
-    exports.paths[type] = path;
-  });
-};
-
-// The following function names are provided to you to suggest how you might
-// modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(cb) {
   client.multi.getKeys().execAsync().then(cb);
