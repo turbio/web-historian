cd226beda741f569ebd5922db3087b96a35039c1
D. Wren <drawnwren@gmail.com>
2016-08-23 23:05:09 -0700

    changed post request to use body-parser

--- web/postHandler.js
+++ web/postHandler.js
@@ -3,19 +3,14 @@ var archive = require('../helpers/archive-helpers');
 var http = require('./http-helpers');
 
 module.exports = function(req, res) {
-  var body = '';
-  req.on('data', (data) => body += data);
+  url = /url=([^&]+)/.exec(req.body);
 
-  req.on('end', () => {
-    url = /url=([^&]+)/.exec(body);
+  if (url === null || !url[1]) {
+    return http.clientErr(res, 'you must include a url');
+  } else {
+    http.redirect(res, '/');
+  }
 
-    if (url === null || !url[1]) {
-      return http.clientErr(res, 'you must include a url');
-    } else {
-      http.redirect(res, '/');
-    }
-
-    console.log('appending', url[1], 'to db');
-    archive.addUrlToList(url[1], _=> {});
-  });
+  console.log('appending', url[1], 'to db');
+  archive.addUrlToList(url[1], _=> {});
 };
