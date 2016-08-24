eb2777cfef67807005c957ea5c6894117e238f5d
D. Wren <drawnwren@gmail.com>
2016-08-24 00:05:36 -0700

    not really sure what I changed

--- web/postHandler.js
+++ web/postHandler.js
@@ -4,12 +4,12 @@ var http = require('./http-helpers');
 
 module.exports = function(req, res) {
 
-    if (!req.body.url) {
-      return http.clientErr(res, 'you must include a url');
-    } else {
-      http.redirect(res, '/');
-    }
+  if (!req.body.url) {
+    return http.clientErr(res, 'you must include a url');
+  } else {
+    http.redirect(res, '/');
+  }
 
-    console.log('appending', req.body.url, 'to db');
-    archive.addUrlToList(req.body.url, _=> {});
+  console.log('appending', req.body.url, 'to db');
+  archive.addUrlToList(req.body.url, _=> {});
 };
